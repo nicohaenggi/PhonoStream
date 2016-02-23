9708cef98630ea21bf6dc589eabf6b5d5d60b281
= <=>
2016-02-23 20:33:12 +0100

    changed misspelled username

--- core/db/index.js
+++ core/db/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
 //	connectionString = "mongodb://localhost/leopard",
-    connectionString = "mongodb://admib:lpic11lpic11@ds015398.mongolab.com:15398/heroku_qsg214kh",
+    connectionString = "mongodb://admin:lpic11lpic11@ds015398.mongolab.com:15398/heroku_qsg214kh",
 	options = { server : { auto_reconnect: true, poolSize: 10 } },
 	Promise = require('bluebird'),
     song = require('./song');
