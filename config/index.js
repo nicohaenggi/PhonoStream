3aa70bb527b47ef45f448123e6592b9c76c2ab92
= <=>
2016-03-10 08:23:19 +0100

    chaged conf

--- config/index.js
+++ config/index.js
@@ -5,39 +5,43 @@ var nconf = require('nconf'),
 * nconf.env('_'): specify a separator for nested keys (instead of the default ':')
 */
 function Config() {
-	nconf.argv();
-	nconf.env('_');
-	var environment = nconf.get('NODE:ENV') || 'development';
-	nconf.file(environment, 'config/' + environment + '.json');
-	nconf.file('default', 'config/default.js');
+    nconf.argv();
+    // setting the required keys for openshift
+    var PORT = process.env.OPENSHIFT_NODEJS_PORT;
+    var IP = process.env.OPENSHIFT_NODEJS_IP;
+    var MONGO = process.env.OPENSHIFT_MONGODB_DB_URL + "phonostream";
+    nconf.overrides({
+        'express': {
+            'port': PORT,
+            'ip': IP
+        },
+        'mongo': {
+            'url': MONGO
+        }
+    });
+
+    nconf.env('_');
+    var environment = nconf.get('NODE:ENV')  || 'development';
+    nconf.file(environment, 'config/' + environment + '.json');
+    nconf.file('default', 'config/default.js');
 }
 
 /** Accessing Values
 * Defining Getter-Method
 */
-Config.prototype.get = function(key) {
-	return nconf.get(key);
+Config.prototype.get = function (key) {
+    return nconf.get(key);
 }
 
-// setting the required keys for openshift
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || nconf.get('express:port');
-var IP = process.env.OPENSHIFT_NODEJS_IP || nconf.get('express:ip');
-nconf.set('express:port', PORT);
-nconf.set('express:ip', IP);
-if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    var MONGO = process.env.OPENSHIFT_MONGODB_DB_URL + "phonostream";
-    nconf.set('mongo:url', MONGO);
-}
-
-console.log(PORT + " , " + IP + " , " + process.env.OPENSHIFT_MONGODB_DB_URL );
+console.log(PORT + " , " + IP + " , " + process.env.OPENSHIFT_MONGODB_DB_URL);
 
 // save new config to file
 nconf.save(function (err) {
-    var environment = nconf.get('NODE:ENV') || 'development';
+    var environment = nconf.get('NODE:ENV')  || 'development';
     fs.readFile('config/' + environment + '.json', function (err, data) {
-      console.log(JSON.parse(data.toString()));
+        console.log(JSON.parse(data.toString()));
     });
-  });
+});
 
 // exporting for use elsewhere
 module.exports = new Config();
