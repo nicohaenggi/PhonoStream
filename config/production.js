7abb98e3c519828da3a75c03d9c29828ed5e3a05
= <=>
2016-03-12 17:46:58 +0100

    changed port

--- config/production.js
+++ config/production.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: "production",
     express: {
-        port: process.env.OPENSHIFT_NODEJS_PORT,
-        ip: process.env.OPENSHIFT_NODEJS_IP
+        port: 3000,
+        ip: "10.135.16.248"
     },
     mongo: {
         url : process.env.OPENSHIFT_MONGODB_DB_URL + "nodejs"
