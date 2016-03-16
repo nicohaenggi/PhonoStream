c8336b533ef29a4cbbd1c0b1c736029a1aca3596
Nico Haenggi <nico@haenggi.ch>
2016-03-16 18:07:18 +0100

    private ip

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
     env: "production",
     express: {
         port: 3000,
-        ip: "46.101.109.42"
+        ip: "10.135.18.48"
     },
     mongo: {
         url : "mongodb://127.0.0.1/" + "phonostream"
